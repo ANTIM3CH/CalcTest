3563ac4f8d1d605518132fb12c046a6f8be0e3c4
ANTIM3CH <austinloganart@gmail.com>
2023-11-20 13:49:20 -0500

    It block edge case separation

--- cypress/e2e/smoke/edge-cases.cy.js
+++ cypress/e2e/smoke/edge-cases.cy.js
@@ -1,5 +1,5 @@
 describe("Edge Cases Test", () => {
-  it("handles edge cases appropriately", () => {
+  it("handles lowest loan amount appropriately", () => {
     cy.visit("https://www.calculator.net/mortgage-calculator.html");
 
     // Uncheck optional fields
@@ -12,11 +12,21 @@ describe("Edge Cases Test", () => {
     cy.getId("cinterestrate").clear().type("1");
     cy.get('input[name="x"][value="Calculate"]').click();
 
+    // Verify the result, check for validity
+    cy.get(".h2result").should("be.visible").and("contain", "0.08");
+
     // Clear input fields
     cy.get('input[name="chouseprice"]').clear();
     cy.get('input[name="cdownpayment"]').clear();
     cy.get('input[name="cloanterm"]').clear();
     cy.get('input[name="cinterestrate"]').clear();
+  });
+
+  it("handles highest loan amount appropriately", () => {
+    cy.visit("https://www.calculator.net/mortgage-calculator.html");
+
+    // Uncheck optional fields
+    cy.get('label[for="caddoptional"]').click().should("not.be.checked");
 
     // Case 2: Highest Loan Amount
     cy.getId("chouseprice").clear().type("999999999");
@@ -25,11 +35,21 @@ describe("Edge Cases Test", () => {
     cy.getId("cinterestrate").clear().type("1");
     cy.get('input[name="x"][value="Calculate"]').click();
 
+    // Verify the result, check for validity
+    cy.get(".h2result").should("be.visible").and("contain", "$83,785,411.47");
+
     // Clear input fields
     cy.get('input[name="chouseprice"]').clear();
     cy.get('input[name="cdownpayment"]').clear();
     cy.get('input[name="cloanterm"]').clear();
     cy.get('input[name="cinterestrate"]').clear();
+  });
+
+  it("handles highest interest rate appropriately", () => {
+    cy.visit("https://www.calculator.net/mortgage-calculator.html");
+
+    // Uncheck optional fields
+    cy.get('label[for="caddoptional"]').click().should("not.be.checked");
 
     // Edge Case 3: Highest Interest Rate
     cy.getId("chouseprice").clear().type("500000");
@@ -38,7 +58,7 @@ describe("Edge Cases Test", () => {
     cy.getId("cinterestrate").clear().type("100");
     cy.get('input[name="x"][value="Calculate"]').click();
 
-    // Verify the result, check for validity based on your expectations
+    // Verify the result, check for validity
     cy.get(".h2result").should("be.visible").and("contain", "$41,666.67");
 
     // Clear input fields
